ec8219fd55bf941d05dde42c1f0cc76c260cbfc1
CemBC <119740765+CemBC@users.noreply.github.com>
2024-04-06 15:43:32 +0300

    appBar and Title
    Creating MaterailApp for material designs , and as a widget creating scaffold with appBar which has indigo blue and title at the center as "First Portfolio Exam"

---
+++ main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(backgroundColor: Colors.indigo , centerTitle: true, title: const Text("First Portfolio Exam",
+      style: TextStyle(
+        color: Colors.white
+      ))),
+
+    )
+  ));
+}
+
+
