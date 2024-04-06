539afe424d8b960c471708692b55b7d5a7da2625
CemBC <119740765+CemBC@users.noreply.github.com>
2024-04-06 23:14:39 +0300

    Adding color from color picker

--- main.dart
+++ main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
-          appBar: AppBar(backgroundColor: Colors.indigo , centerTitle: true, title: const Text("First Portfolio Exam",
+          appBar: AppBar(backgroundColor: Color(0xFF0d3473) , centerTitle: true, title: const Text("First Portfolio Exam",
               style: TextStyle(
                   color: Colors.white
               ))),
