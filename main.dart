533b3f8d5db8743e8d51c9db9be32b5113490349
CemBC <119740765+CemBC@users.noreply.github.com>
2024-04-06 16:11:34 +0300

    Creating 3 boxes
    to Center the 3 boxes used "Center" and align them as a row by "Row".  mainAxis and CrossAxis are center. as a children there are 3 container from left to right and each adjence has sizedbox with width 40

--- main.dart
+++ main.dart
@@ -7,6 +7,19 @@ void main() {
       style: TextStyle(
         color: Colors.white
       ))),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children : [
+            Container(width: 100, height: 100, color: Colors.black),
+              SizedBox(width: 40),
+            Container(width: 100, height: 200, color: Colors.black54),
+              SizedBox(width: 40),
+            Container(width: 100, height:  100 , color: Colors.grey),
+        ],
+      ))
+
 
     )
   ));
