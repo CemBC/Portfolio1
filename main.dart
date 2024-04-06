4f66b7d7d4e7f8381a0e465fc0ea672a2de5c1f9
CemBC <119740765+CemBC@users.noreply.github.com>
2024-04-06 19:51:19 +0300

    Adding last arrangements
    Due to my flutter project crashed instantly, ı had to remove codes to another folder. And Rearranged the sizedBox heights of column

--- main.dart
+++ main.dart
@@ -2,78 +2,77 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(backgroundColor: Colors.indigo , centerTitle: true, title: const Text("First Portfolio Exam",
-      style: TextStyle(
-        color: Colors.white
-      ))),
+      home: Scaffold(
+          appBar: AppBar(backgroundColor: Colors.indigo , centerTitle: true, title: const Text("First Portfolio Exam",
+              style: TextStyle(
+                  color: Colors.white
+              ))),
+          body:Stack(
+            children: [
+              Center(
+                  child: Column(
+                    children: [
+                      SizedBox(height: 75),
+                      Container(height: 100, width: 200,
+                          decoration: BoxDecoration(
+                            color: Colors.deepOrange,
+                            border: Border.all(color: Colors.black, width: 5),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          alignment: Alignment.center,
+                          child: const Text(
+                              "Welcome" ,
+                              style: TextStyle(color: Colors.white , fontWeight: FontWeight.bold,fontSize: 40)
+                          )),
+                      SizedBox(height: 400),
+                      Container(width: 300 , height: 200,
+                          decoration: BoxDecoration(
+                              border: Border.all( color: Colors.grey ,
+                                  width : 2)
+                          ),
+                          child: Image.asset("assets/images/thwsLogo.png",
+                              fit: BoxFit.contain)
+                      )
+                    ],
+                  )
+              )
+              ,
+              Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children : [
+                      Container(width: 100, height: 100, color: Colors.black ,
+                          alignment: Alignment.bottomRight,
+                          child: const Text(
+                              "TAMK" ,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)
+                          )),
+                      SizedBox(width: 40),
+                      Container(width: 100, height: 200, color: Colors.black54,
+                          child: Center(
+                              child: const Text("Flutter!" ,
+                                  style: TextStyle(color: Colors.white,
+                                      fontWeight: FontWeight.bold))
+                          )),
+                      SizedBox(width: 40),
+                      Container(width: 100, height:  100 , color: Colors.grey,
+                          alignment: Alignment.bottomLeft,
+                          child: const Text(
+                              "THWS",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)
+                          ) ),
+                    ],
+                  )
 
-      body:Stack(
-      children: [
-          Center(
-            child: Column(
-              children: [
-                SizedBox(height: 100),
-                Container(height: 100, width: 200,
-                decoration: BoxDecoration(
-                  color: Colors.deepOrange,
-                  border: Border.all(color: Colors.black, width: 5),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                alignment: Alignment.center,
-                child: const Text(
-                  "Welcome" ,
-                  style: TextStyle(color: Colors.white , fontWeight: FontWeight.bold,fontSize: 40)
-                )),
-                SizedBox(height: 375),
-                Container(width: 300 , height: 200,
-                  decoration: BoxDecoration(
-                    border: Border.all( color: Colors.grey ,
-                    width : 5)
-                  ),
-                    child: Image.asset("assets/images/thwsLogo.png",
-                    fit: BoxFit.contain)
-                )
-              ],
-            )
+              )
+            ],
           )
-        ,
-      Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children : [
-            Container(width: 100, height: 100, color: Colors.black ,
-            alignment: Alignment.bottomRight,
-            child: const Text(
-              "TAMK" ,
-                style: TextStyle(
-                    color: Colors.white,
-                fontWeight: FontWeight.bold)
-            )),
-              SizedBox(width: 40),
-            Container(width: 100, height: 200, color: Colors.black54,
-                child: Center(
-                  child: const Text("Flutter!" ,
-                  style: TextStyle(color: Colors.white,
-                  fontWeight: FontWeight.bold))
-                )),
-              SizedBox(width: 40),
-            Container(width: 100, height:  100 , color: Colors.grey,
-            alignment: Alignment.bottomLeft,
-             child: const Text(
-               "THWS",
-               style: TextStyle(
-                 color: Colors.white,
-                 fontWeight: FontWeight.bold)
-             ) ),
-        ],
-      )
-
-      )
-        ],
-    )
-  )));
+      )));
 }
 
 
